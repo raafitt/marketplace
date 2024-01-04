5f1ab3d5a9de42b25257b8bac37c02e8b645f825
raafitt <baymukhametov99@gmail.com>
2024-01-04 13:33:55 +0500

    only backend

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const Router=require('express')
+
+const router=new Router()
+
+const brandRouter=require('./brandRouter')
+const deviceRouter=require('./deviceRouter')
+const typeRouter=require('./typeRouter')
+const userRouter=require('./userRouter')
+
+router.use('/brand',brandRouter)
+router.use('/type',typeRouter)
+router.use('/device',deviceRouter)
+router.use('/user',userRouter)
+
+module.exports=router
