1f82b685c6467874a5bce2df3b7a3695a4859439
raafitt <baymukhametov99@gmail.com>
2024-01-17 21:41:27 +0500

    without basket

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -7,22 +7,24 @@ class DeviceController{
 
     async create(req,res,next){
         try{
-            const {name,price,brandId, typeId, info}=req.body
+            let {name,price,brandId, typeId, info}=req.body
             const {img}=req.files
             let fileName=uuid.v4() + '.jpg'
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-            const device=await Device.create({name,price,brandId, typeId, img:fileName})
-            return res.json(device)
+            const device=await Device.create({name,price,brandId, typeId, img:fileName })
+            
 
-            if(info){
-                info=JSON.parse(info);
-                info.forEach(i=>
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i =>
                     DeviceInfo.create({
-                        title:i.title,
-                        description:i.description,
-                        deviceId:device.id
-                    }))
+                        tittle: i.tittle,
+                        description: i.description,
+                        deviceId: device.id
+                    })
+                )
             }
+            return res.json(device)
         }
          catch(e){
             next(ApiError.badRequest(e.message))
@@ -34,7 +36,7 @@ class DeviceController{
     async getAll(req,res){
         let {brandId, typeId, limit, page}=req.query
         page=page || 1;
-        limit=limit || 9;
+        limit=limit || 5;
         let offset=page*limit-limit;
         let devices;
         if(!brandId && !typeId){
